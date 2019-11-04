b05e46ba5eb640f4bb83de8999fa31647e36a507
Gustavo Felhberg <gustavo.felhberg@gmail.com>
2019-11-04 12:43:08 -0800

    testing

--- start.js
+++ start.js
@@ -6,7 +6,8 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
@@ -15,7 +16,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('index');
+    res.sendFile(__dirname + '/views/index.html');
+    // res.render('index');
 });
 
 app.listen(port, function() {
